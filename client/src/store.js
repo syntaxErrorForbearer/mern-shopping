3a59e97b87cd9f0ab3b55951996f91f09da3c681
syntaxerrorforbearer <syntaxerrorforbearer@gmail.com>
2018-11-19 14:22:28 -0500

    step 6: add Item input

--- client/src/store.js
+++ client/src/store.js
@@ -7,13 +7,15 @@ const initialState = {};
 const middleware = [thunk];
 
 // window._REDUX_DEVTOOLS_EXTENSION_ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
-// was a fix 
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// was a fix
 const store = createStore(
   rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window._REDUX_DEVTOOLS_EXTENSION_ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f  )
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f  )
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
